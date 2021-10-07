82e12f727d1c42b81793818b3ded9073ae1d06c2
SnoopyCodeX <extremeclasherph@gmail.com>
2021-10-08 03:07:31 +0800

    Update transcription-controller.js

--- api/controller/transcription-controller.js
+++ api/controller/transcription-controller.js
@@ -6,16 +6,20 @@ dotenv.config();
 import { RevAiApiClient } from 'revai-node-sdk';
 
 export const transcribeAudio = async (req, res) => {
-    const ACCESS_TOKEN = process.env.REV_AI_ACCESS_TOKEN;
-    let client = new RevAiApiClient(ACCESS_TOKEN);
+    try {
+        const ACCESS_TOKEN = process.env.REV_AI_ACCESS_TOKEN;
+        let client = new RevAiApiClient(ACCESS_TOKEN);
 
-    console.log(req.body);
-    
-    var job = await client.submitJobUrl(req.body.audioUrl);
-    // var details = await client.getJobDetails(job.id);
-    var transcript = await client.getTranscriptText(job.id);
+        console.log(req.body);
+        
+        var job = await client.submitJobUrl(req.body.audioUrl);
+        // var details = await client.getJobDetails(job.id);
+        var transcript = await client.getTranscriptText(job.id);
 
-    console.log(transcript);
+        console.log(transcript);
 
-    res.json({"transcript": transcript});
+        res.json({"transcript": transcript});
+    } catch(e) {
+        console.log(e);
+    }
 };
