6c8553f0df30365a93029107aaec8feddfa9f774
SnoopyCodeX <extremeclasherph@gmail.com>
2021-10-07 21:18:51 +0800

    Initial commit

---
+++ api/controller/transcription-controller.js
@@ -0,0 +1,21 @@
+'use strict';
+
+require('dotenv').config();
+const RevAiApiClient = require('revai-node-sdk');
+
+exports.transcribeAudio = async (req, res) => {
+    const ACCESS_TOKEN = process.env.REV_AI_ACCESS_TOKEN;
+    let client = new RevAiApiClient(ACCESS_TOKEN);
+    let body = JSON.parse(req.body);
+
+    // Submit audio file to Rev.ai
+    var job = await client.submitJobUrl(body.audioUrl);
+
+    // Check your job's status
+    // var details = await client.getJobDetails(job.id);
+
+    // Retrieve transcript
+    var transcript = await client.getTranscriptText(job.id);
+
+    res.json({"transcript": transcript});
+};
---
+++ server.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+
+const express = require('express'),
+      app = express(),
+      port = process.env.PORT || 3000,
+      bodyParser = require('body-parser'),
+      routes = require('./api/routes/app-routes');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+routes(app);
+
+app.use(function(req, res) {
+    res.status(404).send({url: req.originalUrl + ' not found'})
+});
+
+// Listens to server port
+app.listen(port);
